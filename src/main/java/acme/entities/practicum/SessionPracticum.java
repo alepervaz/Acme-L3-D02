f73a4522ba50285407b4457b8706f07e65376738
Alejandro Santiago Félix <alex.0002002@gmail.com>
2023-02-27 20:37:38 +0100

    feat(init): Session entity creation
    Due to the difficulty of one of the constraints, it has been decided to create a class to validate the entity.

---
+++ src/main/java/acme/entities/practicum/SessionPracticum.java
@@ -0,0 +1,63 @@
+
+package acme.entities.practicum;
+
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SessionPracticum extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	private String				code;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				abstractSession; // TODO: Rename
+
+	@NotBlank
+	@Length(max = 101)
+	private String				description;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date start;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date			end;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Valid
+	@ManyToOne(optional = false)
+	private Practicum			practicum;
+
+}
---
+++ src/main/java/acme/entities/practicum/SessionValidator.java
@@ -0,0 +1,30 @@
+
+package acme.entities.practicum;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class SessionValidator implements Validator {
+
+	@Override
+	public boolean supports(final Class<?> clazz) {
+		return SessionPracticum.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(final Object target, final Errors errors) {
+		final SessionPracticum session = (SessionPracticum) target;
+		final ZoneId zoneId = ZoneId.systemDefault();
+		final LocalDateTime start = LocalDateTime.ofInstant(session.getStart().toInstant(), zoneId);
+		final LocalDateTime end = LocalDateTime.ofInstant(session.getEnd().toInstant(), zoneId);
+		final LocalDateTime now = LocalDateTime.now();
+
+		if (start.isBefore(now.plusWeeks(1)))
+			errors.rejectValue("start", "start.beforeNow", "Start date must be at least one week in the future");
+		else if (end.isAfter(start.plusWeeks(1)))
+			errors.rejectValue("end", "end.afterStart", "End date must be at least one week after start");
+	}
+}

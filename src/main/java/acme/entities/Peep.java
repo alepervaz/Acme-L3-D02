29f8725ca650770b3ed5aeaefbd24b2251a38079
sersansan2 <sersansan2@alum.us.es>
2023-02-28 11:10:39 +0100

    Feat(#20): Create role Auditor

---
+++ src/main/java/acme/entities/Peep.java
@@ -0,0 +1,56 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@ToString
+public class Peep extends AbstractEntity {
+
+	// Serialisation identifier
+
+	protected static final long	serialVersionUID	= 1L;
+
+	//Attributes
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				moment;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				nick;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+}
---
+++ src/main/java/acme/roles/Auditor.java
@@ -0,0 +1,40 @@
+
+package acme.roles;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+
+public class Auditor extends AbstractEntity {
+
+	// Serialisation identifier
+
+	protected static final long	serialVersionUID	= 1L;
+
+	//Attributes
+
+	@NotBlank
+	@Length(max = 76)
+	private String				firm;
+
+	@NotBlank
+	@Length(max = 26)
+	private String				proffesionalId;
+
+	@NotNull
+	@Valid
+	@Size(max = 101)
+	private List<String>		certifications;
+
+	@URL
+	private String				link;
+
+}

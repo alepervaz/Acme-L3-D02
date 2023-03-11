387eebeaac81f5b44fa8d5817d9ea1e7b3f1bc5f
Alejandro Santiago Félix <alex.0002002@gmail.com>
2023-03-11 10:47:11 +0100

    fix(update): Admin dashboard creation
    The statistics values cannot be negative.

--- src/main/java/acme/forms/AdminDashboard.java
+++ src/main/java/acme/forms/AdminDashboard.java
@@ -7,6 +7,9 @@ import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 public class AdminDashboard extends AbstractForm {
@@ -18,17 +21,25 @@ public class AdminDashboard extends AbstractForm {
 	// Attributes -------------------------------------------------------------
 
 	// Total number of principals with each role
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfAssistants;
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfAuditors;
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfCompanys;
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfConsumers;
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfProviders;
+	@Positive(message = "Number of administrators must be positive")
 	private Integer						numberOfLecturers;
 
 	// Ratio of peeps with both an email address and a link
+	@Positive(message = "Ratio of peeps with both an email address and a link must be positive")
 	private Double					linkAndEmailPeepsRatio;
 
 	// Ratios of critical and non-critical bulletins
+	@Positive(message = "Ratio of critical bulletins must be positive")
 	private Double					criticalBulletinsRatio;
 
 	// Average, minimum, maximum, and standard deviation of the budget in the offers grouped by currency
@@ -38,9 +49,13 @@ public class AdminDashboard extends AbstractForm {
 	private Map<String, Double>		standardDeviationBudgetByCurrency;
 
 	// Average, minimum, maximum, and standard deviation of the number of notes posted over the last 10 weeks
+	@Positive(message = "Average number of notes posted over the last 10 weeks must be positive")
 	private Double					averageNotesInLast10Weeks;
+	@Positive(message = "Minimum number of notes posted over the last 10 weeks must be positive")
 	private Double					minimumNotesInLast10Weeks;
+	@Positive(message = "Maximum number of notes posted over the last 10 weeks must be positive")
 	private Double					maximumNotesInLast10Weeks;
+	@Positive(message = "Standard deviation of the number of notes posted over the last 10 weeks must be positive")
 	private Double					standardDeviationNotesInLast10Weeks;
 
 	// Derived attributes -----------------------------------------------------

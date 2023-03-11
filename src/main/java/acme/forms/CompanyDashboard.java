046cc68a1da65c72df8bbc6a79c16c42e54796ff
Alejandro Santiago Félix <alex.0002002@gmail.com>
2023-03-11 10:45:33 +0100

    fix(update): Company dashboard creation
    The statistics values cannot be negative.

--- src/main/java/acme/forms/CompanyDashboard.java
+++ src/main/java/acme/forms/CompanyDashboard.java
@@ -6,6 +6,8 @@ import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 public class CompanyDashboard extends AbstractForm {
@@ -20,16 +22,24 @@ public class CompanyDashboard extends AbstractForm {
 	private List<Integer>		totalNumberOfPracticaByMonth;
 
 	// Average, deviation, minimum, and maximum period length of the sessions in their practica.
+	@Positive(message = "The average session length must be positive")
 	private Double				averageSessionLength;
+	@Positive(message = "The deviation session length must be positive")
 	private Double				deviationSessionLength;
+	@Positive(message = "The minimum session length must be positive")
 	private Double				minimumSessionLength;
+	@Positive(message = "The maximum session length must be positive")
 	private Double				maximumSessionLength;
 
 
 	// Average, deviation, minimum, and maximum period length of their practica.
+	@Positive(message = "The average practica length must be positive")
 	private Double				averagePracticaLength;
+	@Positive(message = "The deviation practica length must be positive")
 	private Double				deviationPracticaLength;
+	@Positive(message = "The minimum practica length must be positive")
 	private Double				minimumPracticaLength;
+	@Positive(message = "The maximum practica length must be positive")
 	private Double				maximumPracticaLength;
 
 	// Derived attributes -----------------------------------------------------

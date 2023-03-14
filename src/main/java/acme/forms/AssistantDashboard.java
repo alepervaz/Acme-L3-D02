035e9837d7aa5dd1ffc0538211531da606c73d31
alepervaz <alepervaz@alum.us.es>
2023-03-14 18:17:04 +0100

    feat(init):Assistant dashboard creation

---
+++ src/main/java/acme/forms/AssistantDashboard.java
@@ -0,0 +1,48 @@
+
+package acme.forms;
+
+import java.util.List;
+
+import javax.validation.constraints.Positive;
+
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssistantDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Total number of tutorial (the index in the list represent type of tutorial).
+	private List<Integer>		totalNumberOfTutorials;
+
+	// Average, deviation, minimum, and maximum time length of his or her sessions.
+	@Positive(message = "The average session length must be positive")
+	private Double				averageSession;
+	@Positive(message = "The deviation session length must be positive")
+	private Double				deviationSession;
+	@Positive(message = "The minimum session length must be positive")
+	private Double				minimumSession;
+	@Positive(message = "The maximum session length must be positive")
+	private Double				maximumSession;
+
+	// Average, deviation, minimum, and maximum time length of his or her tutorial.
+	@Positive(message = "The average practica length must be positive")
+	private Double				averageTutorial;
+	@Positive(message = "The deviation practica length must be positive")
+	private Double				deviationTutorial;
+	@Positive(message = "The minimum practica length must be positive")
+	private Double				minimumTutorial;
+	@Positive(message = "The maximum practica length must be positive")
+	private Double				maximumTutorial;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}

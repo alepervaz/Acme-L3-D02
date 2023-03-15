301a47851eb743ed956966494905bf2160b598bc
alepervaz <alepervaz@alum.us.es>
2023-03-15 01:32:42 +0100

    fix(update): bulletin Entity atributes

--- src/main/java/acme/entities/bulletin.java
+++ src/main/java/acme/entities/bulletin.java
@@ -1,9 +1,12 @@
 
-package entities;
+package acme.entities;
 
 import java.util.Date;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
@@ -11,23 +14,26 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
 
-public class Bulletin extends AbstractEntity {
+public class bulletin extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+	@NotNull
 	@Past
-	protected Date				moment;
+	@Temporal(TemporalType.DATE)
+	protected Date				moment;;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75, message = "title shorter than 76 characters")
 	protected String			title;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100, message = "message shorter than 101 characters")
 	protected String			message;
 
+	@NotNull
 	protected boolean			flags;
 
 	@URL

e06edb8a0d0d5f4806d10641d2bbdc9f190cf651
marvicmar <marvicmar@alum.us.es>
2023-03-15 18:35:57 +0100

    Update Student Dashboard

--- src/main/java/acme/forms/StudentDashboard.java
+++ src/main/java/acme/forms/StudentDashboard.java
@@ -3,8 +3,6 @@ package acme.forms;
 
 import java.util.List;
 
-import javax.validation.constraints.Positive;
-
 import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,23 +21,15 @@ public class StudentDashboard extends AbstractForm {
 	private List<Integer>		totalNumberOfActivities;
 
 	//Average, deviation, minimum, and maximum period of the activities in his or her workbook;
-	@Positive(message = "The average activities length must be positive")
 	private Double				averageAtivities;
-	@Positive(message = "The deviation activities length must be positive")
 	private Double				deviationAtivities;
-	@Positive(message = "The minimum activities length must be positive")
 	private Double				minimumAtivities;
-	@Positive(message = "The maximum activities length must be positive")
 	private Double				maximumAtivities;
 
 	// Average, deviation, minimum, and maximum time length of his or her enrolled.
-	@Positive(message = "The average enrolled length must be positive")
 	private Double				averageEnrolled;
-	@Positive(message = "The deviation enrolled length must be positive")
 	private Double				deviationEnrolled;
-	@Positive(message = "The minimum enrolled length must be positive")
 	private Double				minimumEnrolled;
-	@Positive(message = "The maximum enrolled length must be positive")
 	private Double				maximumEnrolled;
 
 	// Derived attributes -----------------------------------------------------

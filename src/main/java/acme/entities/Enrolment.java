242754cb806192096571f2ab1e023e46dcf544cb
marvicmar <marvicmar@alum.us.es>
2023-03-15 18:55:30 +0100

    Add entity enrolment

---
+++ src/main/java/acme/entities/Enrolment.java
@@ -0,0 +1,56 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import acme.roles.Student;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Enrolment extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}$")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			motivation;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	// Derived attributes -----------------------------------------------------
+	//work time (in hours, computed from the corresponding activities)
+
+	// Relationships -------------------------------------------------------------
+	@Valid
+	@NotNull
+	@ManyToOne()
+	protected Student			student;
+
+	@Valid
+	@NotNull
+	@ManyToOne()
+	protected Activites			activites;
+}

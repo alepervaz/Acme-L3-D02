bad7e7cdee13606b52678f7db38f86f4afd7f6bf
alepervaz <alepervaz@alum.us.es>
2023-03-16 01:27:42 +0100

    fix(update): anotaciones

--- src/main/java/acme/entities/Peep.java
+++ src/main/java/acme/entities/Peep.java
@@ -3,6 +3,7 @@ package acme.entities;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
@@ -14,7 +15,12 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Peep extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------

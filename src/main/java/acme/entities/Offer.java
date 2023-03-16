bc86fc3df3da673f67d5119dc68af910b35a2cc4
María Vico <73558808+marvicmar@users.noreply.github.com>
2023-03-16 08:50:19 +0100

    Update Offer.java
    Add @Getter, @Setter y @Entity

--- src/main/java/acme/entities/Offer.java
+++ src/main/java/acme/entities/Offer.java
@@ -3,6 +3,7 @@ package acme.entities;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
@@ -14,7 +15,12 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Offer extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------

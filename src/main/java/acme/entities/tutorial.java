eebb950143cb82bd9150303cb73007de80c8ac9e
alepervaz <alepervaz@alum.us.es>
2023-03-16 01:32:19 +0100

    fix(update):anotaciones

--- src/main/java/acme/entities/tutorial.java
+++ src/main/java/acme/entities/tutorial.java
@@ -1,6 +1,7 @@
 
 package acme.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -11,7 +12,12 @@ import javax.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class tutorial extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 

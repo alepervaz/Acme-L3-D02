eb3f8bb84553af27ec124ce36f1ca8bffff0a6cb
marvicmar <marvicmar@alum.us.es>
2023-03-17 17:49:16 +0100

    Change in StudentDashboard

--- src/main/java/acme/forms/StudentDashboard.java
+++ src/main/java/acme/forms/StudentDashboard.java
@@ -1,8 +1,9 @@
 
 package acme.forms;
 
-import java.util.List;
+import java.util.Map;
 
+import acme.entities.enums.Approach;
 import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,24 +14,24 @@ public class StudentDashboard extends AbstractForm {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long		serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	// Total number of tutorial (the index in the list represent type of tutorial).
-	private List<Integer>		totalNumberOfActivities;
+	private Map<Approach, Integer>	totalNumberOfActivities;
 
 	//Average, deviation, minimum, and maximum period of the activities in his or her workbook;
-	private Double				averageAtivities;
-	private Double				deviationAtivities;
-	private Double				minimumAtivities;
-	private Double				maximumAtivities;
+	private Double					averageAtivities;
+	private Double					deviationAtivities;
+	private Double					minimumAtivities;
+	private Double					maximumAtivities;
 
 	// Average, deviation, minimum, and maximum time length of his or her enrolled.
-	private Double				averageEnrolled;
-	private Double				deviationEnrolled;
-	private Double				minimumEnrolled;
-	private Double				maximumEnrolled;
+	private Double					averageEnrolled;
+	private Double					deviationEnrolled;
+	private Double					minimumEnrolled;
+	private Double					maximumEnrolled;
 
 	// Derived attributes -----------------------------------------------------
 

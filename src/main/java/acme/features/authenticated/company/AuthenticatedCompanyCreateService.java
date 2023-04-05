885b40ebc314ccf9fbd381bda7a897e842c70c18
Alejandro Santiago Félix <alex.0002002@gmail.com>
2023-04-06 00:08:44 +0200

    feat(init): Anonymous user operations on user accounts #14
    Added operations to add the role Company.

---
+++ src/main/java/acme/features/authenticated/company/AuthenticatedCompanyCreateService.java
@@ -0,0 +1,90 @@
+package acme.features.authenticated.company;
+
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.accounts.Principal;
+import acme.framework.components.accounts.UserAccount;
+import acme.framework.components.models.Tuple;
+import acme.framework.controllers.HttpMethod;
+import acme.framework.helpers.PrincipalHelper;
+import acme.framework.services.AbstractService;
+import acme.roles.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticatedCompanyCreateService extends AbstractService<Authenticated, Company> {
+
+    // Constants -------------------------------------------------------------
+    public static final String[] PROPERTIES = {"name", "vatNumber", "summary", "link"};
+
+    // Internal state ---------------------------------------------------------
+    @Autowired
+    protected AuthenticatedCompanyRepository repository;
+
+    // AbstractService interface ----------------------------------------------
+    @Override
+    public void check() {
+        super.getResponse().setChecked(true);
+    }
+
+    @Override
+    public void authorise() {
+        boolean status;
+
+        status = !super.getRequest().getPrincipal().hasRole(Company.class);
+
+        super.getResponse().setAuthorised(status);
+    }
+
+    @Override
+    public void load() {
+        Company object;
+        Principal principal;
+        int userAccountId;
+        UserAccount userAccount;
+
+        principal = super.getRequest().getPrincipal();
+        userAccountId = principal.getAccountId();
+        userAccount = this.repository.findOneUserAccountById(userAccountId);
+
+        object = new Company();
+        object.setUserAccount(userAccount);
+
+        super.getBuffer().setData(object);
+    }
+
+    @Override
+    public void bind(final Company company) {
+        assert company != null;
+
+        super.bind(company, PROPERTIES);
+    }
+
+    @Override
+    public void validate(final Company company) {
+        assert company != null;
+    }
+
+    @Override
+    public void perform(final Company company) {
+        assert company != null;
+
+        this.repository.save(company);
+    }
+
+    @Override
+    public void unbind(final Company company) {
+        assert company != null;
+
+        Tuple tuple;
+
+        tuple = super.unbind(company, PROPERTIES);
+        super.getResponse().setData(tuple);
+    }
+
+    @Override
+    public void onSuccess() {
+        if (super.getRequest().getMethod().equals(HttpMethod.POST))
+            PrincipalHelper.handleUpdate();
+    }
+}

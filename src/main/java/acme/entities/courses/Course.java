5d7793d7ecd13c4c5b52f082db6230f4f297f829
marvicmar <marvicmar@alum.us.es>
2023-04-13 13:30:33 +0200

    fix(update): the class course and lecture

--- src/main/java/acme/entities/courses/Course.java
+++ src/main/java/acme/entities/courses/Course.java
@@ -3,15 +3,19 @@ package acme.entities.courses;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.entities.enums.Approach;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Lecturer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,6 +31,7 @@ public class Course extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@NotBlank
 	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
 	private String				code;
 
 	@NotBlank
@@ -46,8 +51,13 @@ public class Course extends AbstractEntity {
 	@NotNull
 	protected Approach			type;
 
+	protected boolean			draftMode;
+
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Lecturer			lecturer;
 }
--- src/main/java/acme/entities/courses/Lecture.java
+++ src/main/java/acme/entities/courses/Lecture.java
@@ -2,7 +2,6 @@
 package acme.entities.courses;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -11,7 +10,6 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.entities.enums.Type;
 import acme.framework.data.AbstractEntity;
-import acme.roles.Lecturer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -51,8 +49,5 @@ public class Lecture extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-	@NotNull
-	@ManyToOne
-	protected Lecturer			lecturer;
 
 }
